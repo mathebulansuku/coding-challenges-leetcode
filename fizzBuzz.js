cd2e8e35f7f616217bc1234b3a702b036162467b
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-08 15:40:02 +0200

    fizzBuzz challenge complete

--- fizzBuzz.js
+++ fizzBuzz.js
@@ -4,6 +4,12 @@ function fizzBuzz() {
   for (i = 1; i < 101; i++) {
     if (i % 3 === 0 && i % 5 === 0) {
       output.push("FizzBuzz");
+    } else if (i % 3 === 0) {
+      output.push("Fizz");
+    } else if (i % 5 === 0) {
+      output.push("Buzz");
+    } else {
+      output.push(i);
     }
   }
   console.log(output);
