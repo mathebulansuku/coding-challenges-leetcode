618826db575e74212c19f6bab7366bba2079e7d0
Nsuku Mathebula <mathebulansuku@gmail.com>
2025-03-09 08:10:01 +0200

    complete project

--- fibonacci.js
+++ fibonacci.js
@@ -1,7 +1,7 @@
 function fibonacciGenerator(n) {
   let output = [];
 
-  for (i = 0; i < n; i++) {
+  for (let i = 0; i < n; i++) {
     if (i === 0) {
       output.push(0);
     } else if (i === 1) {
